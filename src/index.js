df6adde0fc292eac3d9577cf1d201cba74c0265b
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-04-15 12:39:11 +0300

    The DOM TaskLister Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  let input=document.querySelector("input[type='text']");
+  let list = document.getElementById("tasks");
+  let form = document.getElementById("create-task-form")
+
+  const handlesubmission = function(){
+    const text = document.createElement("li");
+    text.textContent = input.value;
+    list.append(text);
+  }
+
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    handlesubmission();
+  })
+
+
 });
+
+
